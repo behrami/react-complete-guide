4f5c940e62c601504f7bb328c03ac9faec83e4f8
behrami <behram1993@gmail.com>
2021-07-25 08:31:54 -0400

    added new variables and changed hardcoded jsx to take in dynamic values from new variables

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 7, 25);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 203.05;
+
   return (
     <div className="expense-item">
-      <div>July 10th 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$203.05</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
