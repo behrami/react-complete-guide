a868a992330fba26efa40b7be67b1dcd0dafaa27
behrami <behram1993@gmail.com>
2021-07-25 16:03:41 -0400

    finished setting up expenses component

--- src/App.js
+++ src/App.js
@@ -1,55 +1,10 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "New TV",
-      amount: 799.99,
-      date: new Date(2021, 1, 14),
-    },
-    {
-      id: "e2",
-      title: "Car Insurance",
-      amount: 203.05,
-      date: new Date(2021, 7, 25),
-    },
-    {
-      id: "e3",
-      title: "New Desk (Wooden)",
-      amount: 105.56,
-      date: new Date(2021, 2, 14),
-    },
-    {
-      id: "e4",
-      title: "Toilet Paper",
-      amount: 50.55,
-      date: new Date(2021, 3, 29),
-    },
-  ];
   return (
     <div>
       <h2>Expense Calculator</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses />
     </div>
   );
 }
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,1 +1,58 @@
-function Expenses() {}
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "New TV",
+      amount: 799.99,
+      date: new Date(2021, 1, 14),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 203.05,
+      date: new Date(2021, 7, 25),
+    },
+    {
+      id: "e3",
+      title: "New Desk (Wooden)",
+      amount: 105.56,
+      date: new Date(2021, 2, 14),
+    },
+    {
+      id: "e4",
+      title: "Toilet Paper",
+      amount: 50.55,
+      date: new Date(2021, 3, 29),
+    },
+  ];
+
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
+    </div>
+  );
+}
+
+export default Expenses;
