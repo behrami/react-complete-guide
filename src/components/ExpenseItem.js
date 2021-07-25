87f45a89cd15ca9b057267dde81007c33a5d8d0a
behrami <behram1993@gmail.com>
2021-07-25 14:03:28 -0400

    create expenseDate css and added premade css styling into it

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,9 +2,11 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
+  const date = props.date;
+
   return (
     <div className="expense-item">
-      <ExpenseDate date={props.date} />
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
