67c5730751c524f8533db146974a27120fd0158e
behrami <behram1993@gmail.com>
2021-07-28 11:52:31 -0400

    now data is passed up the components from the form to the app.js file on submit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
+  const addExpenseHandler = (expense) => {
+    console.log("In app.js");
+    console.log(expense);
+  };
   return (
     <div>
       <h2>Expense Calculator</h2>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses />
     </div>
   );
